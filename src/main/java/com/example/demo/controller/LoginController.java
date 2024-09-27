976b079e78baa09c1c03accd5b9e16759fd7fcca
miyairyoto <ryotom3813@outlook.jp>
2024-09-27 20:27:59 +0900

    ログイン時のエラーメッセージを実装

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.context.MessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.constant.MessageConst;
 import com.example.demo.form.LoginForm;
 import com.example.demo.service.LoginService;
+import com.example.demo.util.AppUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +24,8 @@ public class LoginController {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final MessageSource messageSource;
+
     @GetMapping("/login")
     public String view(Model model, LoginForm form) {
          return "login";
@@ -33,7 +38,8 @@ public class LoginController {
         if(isCorrectUserAuth){
             return "redirect:/menu";
         }else{
-            model.addAttribute("errorMsg", "ログインIDとパスワードが間違っています。");
+            var errorMsg = AppUtil.getMessage(messageSource, MessageConst.LOGIN_WRONG_INPUT);
+            model.addAttribute("errorMsg", errorMsg);
             return "login";
         }
     }
