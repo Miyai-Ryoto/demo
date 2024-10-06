3589cb869d5fd7a7eb0a28232a5c368b4d41cdf4
miyairyoto <ryotom3813@outlook.jp>
2024-10-06 20:28:48 +0900

    AnswerInfoの作成

---
+++ src/main/java/com/example/demo/entity/AnswerInfo.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "answer_table")
+public class AnswerInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserInfo userInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostInfo postInfo;
+
+}
