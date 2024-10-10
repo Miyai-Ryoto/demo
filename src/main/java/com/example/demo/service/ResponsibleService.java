a9751b538623a8baeba991aaea3f575b9ac70d8e
miyairyoto <ryotom3813@outlook.jp>
2024-10-10 17:43:45 +0900

    担当者の設定方法をPostsInfoに統一する方法で変更

--- src/main/java/com/example/demo/service/ResponsibleService.java
+++ src/main/java/com/example/demo/service/ResponsibleService.java
@@ -3,11 +3,9 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.PostsInfo;
-import com.example.demo.entity.ResponsibleInfo;
 import com.example.demo.entity.UserInfo;
 import com.example.demo.form.ResponsibleForm;
 import com.example.demo.repository.PostsInfoRepository;
-import com.example.demo.repository.ResponsibleInfoRepository;
 import com.example.demo.repository.UserInfoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,16 +18,11 @@ public class ResponsibleService {
 
     private final PostsInfoRepository postsInfoRepository;
 
-    private final ResponsibleInfoRepository responsibleInfoRepository;
-
     public void resistResponsibleInfo(ResponsibleForm form, Long id){
-
-        ResponsibleInfo responsibleInfo = new ResponsibleInfo();
-        UserInfo userInfo = userInfoRepository.findById(form.getUserId()).orElse(null);
-        responsibleInfo.setUserInfo(userInfo);
         PostsInfo postsInfo = postsInfoRepository.findById(id).orElse(null);
-        responsibleInfo.setPostsInfo(postsInfo);
-        responsibleInfoRepository.save(responsibleInfo);
+        UserInfo userInfo = userInfoRepository.findById(form.getUserId()).orElse(null);
+        postsInfo.setUserInfo(userInfo);
+        postsInfoRepository.save(postsInfo);
     }
 
 }
