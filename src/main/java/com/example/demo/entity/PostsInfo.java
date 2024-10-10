91a4535dce0eda5741401c1a7290b32e92283392
miyairyoto <ryotom3813@outlook.jp>
2024-10-10 17:16:04 +0900

    PostsInfoからResponsibleInfoとの接続を削除し、外部キーにUserInfoを設定する

--- src/main/java/com/example/demo/entity/PostsInfo.java
+++ src/main/java/com/example/demo/entity/PostsInfo.java
@@ -1,15 +1,11 @@
 package com.example.demo.entity;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -30,9 +26,8 @@ public class PostsInfo {
     @JoinColumn(name = "department_id")
     private DepartmentInfo departmentInfo;
 
-    @OneToMany(mappedBy = "postsInfo" , cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ResponsibleInfo> responsibleInfos;
-
-
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserInfo userInfo;
 
 }
