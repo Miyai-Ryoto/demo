efbcb5e171d1a8d79d77d8ec2af3b3af8f6ffa12
miyairyoto <ryotom3813@outlook.jp>
2024-10-23 20:06:23 +0900

    PostFormに文字数のバリデーションを追加

--- src/main/java/com/example/demo/form/PostForm.java
+++ src/main/java/com/example/demo/form/PostForm.java
@@ -7,15 +7,18 @@ import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PostForm {
 
     @NotBlank
+    @Size(min = 5, max = 40)
     private String title;
 
     @NotBlank
+    @Size(min = 50, max = 1000)
     private String content;
 
     @NotNull
