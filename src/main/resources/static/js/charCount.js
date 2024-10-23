e7283feed7a0eaefeaf745e76dd74163308379a0
miyairyoto <ryotom3813@outlook.jp>
2024-10-23 20:32:24 +0900

    文字数をカウントするためのjsファイルを作成

---
+++ src/main/resources/static/js/charCount.js
@@ -0,0 +1,10 @@
+function updateCount(textAreaId, countDisplayId) {
+    const textArea = document.getElementById(textAreaId);
+    const countDisplay = document.getElementById(countDisplayId);
+    const maxLength = textArea.getAttribute("maxlength");
+    countDisplay.textContent = `${textArea.value.length} / ${maxLength}`;
+}
+
+window.onload = function() {
+    updateCount("userComment", "charCount"); // ページロード時に初期値を設定
+};
